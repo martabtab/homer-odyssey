f2134c4c8cf0009084249b138b58695dc1630b80
martabtab <57708251+martabtab@users.noreply.github.com>
2020-04-06 12:10:19 +0200

    quest020

--- front/src/components/SignUp.js
+++ front/src/components/SignUp.js
@@ -3,25 +3,47 @@ import React, { Component } from "react";
 class SignUp extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = {
+      email: "",
+      password: "",
+      name: "",
+      lastname: "",
+    };
 
     this.updateEmailField = this.updateEmailField.bind(this);
+    this.updateField = this.updateField.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  updateField(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
   }
 
   updateEmailField(event) {
     this.setState({ value: event.target.value });
   }
 
+  handleSubmit(event) {
+    console.log(this.state);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
-        <h1>{this.state.value}</h1>
-        <input
-          type="email"
-          name="email"
-          value={this.state.value}
-          onChange={this.updateEmailField}
-        />
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+        <form>
+          <input type="email" name="email" onChange={this.updateField} />
+          <input type="text" name="password" onChange={this.updateField} />
+          <input type="text" name="name" onChange={this.updateField} />
+          <input type="text" name="lastname" onChange={this.updateField} />
+          <input type="submit" value="Submit" onClick={this.handleSubmit} />
+        </form>
       </div>
     );
   }
